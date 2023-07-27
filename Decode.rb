20df3666ed73986e5dfb8117b52b9a4ed7f5deb8
ClintonYounge <clintonyounge18@gmail.com>
2023-07-27 10:28:47 -0400

    Update methods to be more concise

--- Decode.rb
+++ Decode.rb
@@ -1,67 +1,25 @@
 def decode_morse_char(morse_char)
-    case morse_char
-        when ".-"
-          return "A"
-        when "-..."
-          return "B"
-        when "-.-."
-          return "C"
-        when "-.."
-          return "D"
-        when "."
-          return "E"
-        when "..-."
-          return "F"
-        when "--."
-          return "G"
-        when "...."
-          return "H"
-        when ".."
-          return "I"
-        when ".---"
-          return "J"
-        when "-.-"
-          return "K"
-        when ".-.."
-          return "L"
-        when "--"
-          return "M"
-        when "-."
-          return "N"
-        when "---"
-          return "O"
-        when ".--."
-          return "P"
-        when "--.-"
-          return "Q"
-        when ".-."
-          return "R"
-        when "..."
-          return "S"
-        when "-"
-          return "T"
-        when "..-"
-          return "U"
-        when "...-"
-          return "V"
-        when ".--"
-          return "W"
-        when "-..-"
-          return "X"
-        when "-.--"
-          return "Y"
-        else
-          return "Z"
-      end
+  morse_code_to_char = {
+    '.-' => 'A', '-...' => 'B',
+    '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F',
+    '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N',
+    '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R',
+    '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V',
+    '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '..--' => 'Z'
+  }
+  morse_code_to_char[morse_char] || 'UNKNOWN'
 end
 
 def decode_morse_word(morse_word)
-    decoded_word = ""
-    morse_word.split.each do |morse_char|
-        decoded_word += decode_morse_char(morse_char)
-    end
-    return decoded_word
+  morse_word.split.map { |morse_char| decode_morse_char(morse_char) }.join
 end
 
-puts decode_morse_char(".-")
-puts decode_morse_word("-- -.--")
+puts decode_morse_char('.-')
+puts decode_morse_word('-- -.--')
